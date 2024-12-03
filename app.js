3a40db73022c9f94a11c6e439c4c2e1c925a1402
Murun saikhan <zezitimon711@gmail.com>
2024-12-03 18:48:00 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,151 @@
+// 3 ТООНЫ ИХ
+// let too1 = 1;
+// let too2 = 3;
+// let too3 = 2;
+// if(too1 > too2){
+//     if (too1 > too3) {
+//         console.log(too1 + ' ih too');
+//     } else {
+//         console.log(too3 + ' ih too');
+//     }
+// } else {
+//     if(too2 > too3){
+//         console.log(too2 + ' ih too');
+//     } else {
+//         console.log(too3 + " ih too");
+//     }
+// }
+// 4 ТООНЫ БАГА
+// let too1 = 10;
+// let too2 = 11;
+// let too3 = 22;
+// let too4 = 4;
+
+// if(too1<too2){
+//     if(too1<too3){
+//         if(too1<too4){
+//             console.log( too1 + ' baga too');   
+//         }
+//     } 
+// } 
+
+// if(too2<too1){
+//     if(too2<too3){
+//         if(too2<too4){
+//             console.log( too2 + ' baga too');   
+//         }
+//     } 
+// } 
+// if(too3<too1){
+//     if(too3<too2){
+//         if(too3<too4){
+//             console.log( too3 + ' baga too');   
+//         }
+//     } 
+// } 
+// if(too4<too1){
+//     if(too4<too3){
+//         if(too4<too2){
+//             console.log( too4 + ' baga too');   
+//         }
+//     } 
+// } 
+// НИЙЛБЭР 80
+// let  a = 85;
+// let b = 75;
+// let c = 96;
+// let d = 69;
+// let s = 0;
+// if(a > 80) console.log( s = s + a);
+// if(b > 80) console.log( s = s + b);
+// if(c > 80) console.log( s = s + c);
+// if(d > 80) console.log( s = s + d);
+// ҮРЖВЭР 5
+// let  a = 3;
+// let b = 6;
+// let c = 2;
+// let d = 4;
+// let s = 1;
+// if(a < 5) console.log( s = s * a);
+// if(b < 5) console.log( s = s * b);
+// if(c < 5) console.log( s = s * c);
+// if(d < 5) console.log( s = s * d);
+// ТЭГШ ТООНУУДЫН НИЙЛБЭР
+// let  a = 10;
+// let b = 3;
+// let c = 5;
+// let s = 0;
+// if(a % 2 == 0) console.log( s = s + a);
+// if(b % 2 == 0) console.log( s = s + b);
+// if(c % 2 == 0) console.log( s = s + c);
+// СОНДГОЙ ТООНУУДЫН ҮРЖВЭР
+// let  a = 5;
+// let b = 2;
+// let c = 3;
+// let s = 1;
+// if(a % 2 == 1) s = s * a;
+// if(b % 2 == 1) s = s * b;
+// if(c % 2 == 1) s = s * c;
+// console.log( s );
+// Тэнцүү 5
+// let  a = 2;
+// let b = 5;
+// let c = 5;
+// let s = 0;
+// if(a == 5)  s = s + 1;
+// if(b == 5)  s = s + 1;
+// if(c == 5) s = s + 1;
+// console.log(s);
+// 3 -т ХУВААГДАХ
+// let  a = 3;
+// let b = 12;
+// let c = 8;
+// let d = 9;
+// let s = 0;
+// if(a % 3 == 0)  s++;
+// if(b % 3 == 0)  s++;
+// if(c % 3 == 0) s++;
+// if(d % 3 == 0) s++;
+// console.log(s);
+// 11 -т ХУВААГДАДГҮЙ ТООНУУДЫН НИЙЛБЭР
+// let  a = 7;
+// let b = 22;
+// let c = 13;
+// let d = 30;
+// let s = 0;
+// if(a % 11 !== 0)  s = s + a;
+// if(b % 11 !== 0) s = s + b;
+// if(c % 11 !== 0) s = s + c;
+// if(d % 11 !== 0) s = s + d;
+// console.log(s);
+// 10-с ИХ БОЛ YES ҮГҮЙ БОЛ NO
+// let  a = 11;
+// if(a > 10 ) console.log("YES");
+// else {console.log("NO");}
+// 5-с БАГА БОЛ YES ҮГҮЙ БОЛ NO
+// let  a = 5;
+// if(a > 5 ) console.log("YES");
+// else {console.log("NO");}
+// Тэгш ТООНУУД YES
+let  a = 3;
+let b = 4;
+let c = 5;
+if(a % 2 == 0) console.log("YES");
+else {console.log("NO");
+}
+if(b % 2 == 0) console.log("YES");
+else {console.log("NO");
+}
+if(c % 2 == 0) console.log("YES");
+else {console.log("NO");
+}
+
+
+
+
+
+
+
+
+
+
