1674ac15618be171012191b3bbb5be338da9345e
Murun saikhan <zezitimon711@gmail.com>
2024-12-04 12:45:32 +0800

    nemelt

--- app.js
+++ app.js
@@ -15,6 +15,7 @@
 //         console.log(too3 + " ih too");
 //     }
 // }
+
 // 4 ТООНЫ БАГА
 // let too1 = 10;
 // let too2 = 11;
@@ -51,33 +52,40 @@
 //     } 
 // } 
 // НИЙЛБЭР 80
+
 // let  a = 85;
 // let b = 75;
 // let c = 96;
 // let d = 69;
 // let s = 0;
-// if(a > 80) console.log( s = s + a);
-// if(b > 80) console.log( s = s + b);
-// if(c > 80) console.log( s = s + c);
-// if(d > 80) console.log( s = s + d);
+// if(a > 80) s++;
+// if(b > 80) s++;
+// if(c > 80) s++;
+// if(d > 80) s++;
+// console.log( s );
+
 // ҮРЖВЭР 5
 // let  a = 3;
 // let b = 6;
 // let c = 2;
 // let d = 4;
 // let s = 1;
-// if(a < 5) console.log( s = s * a);
-// if(b < 5) console.log( s = s * b);
-// if(c < 5) console.log( s = s * c);
-// if(d < 5) console.log( s = s * d);
+// if(a < 5) s++;
+// if(b < 5) s++;
+// if(c < 5) s++;
+// if(d < 5) s++;
+// console.log( s );
+
 // ТЭГШ ТООНУУДЫН НИЙЛБЭР
 // let  a = 10;
 // let b = 3;
 // let c = 5;
 // let s = 0;
-// if(a % 2 == 0) console.log( s = s + a);
-// if(b % 2 == 0) console.log( s = s + b);
-// if(c % 2 == 0) console.log( s = s + c);
+// if(a % 2 == 0) s++;
+// if(b % 2 == 0) s++;
+// if(c % 2 == 0) s++;
+// console.log( s );
+
 // СОНДГОЙ ТООНУУДЫН ҮРЖВЭР
 // let  a = 5;
 // let b = 2;
@@ -87,6 +95,7 @@
 // if(b % 2 == 1) s = s * b;
 // if(c % 2 == 1) s = s * c;
 // console.log( s );
+
 // Тэнцүү 5
 // let  a = 2;
 // let b = 5;
@@ -96,6 +105,7 @@
 // if(b == 5)  s = s + 1;
 // if(c == 5) s = s + 1;
 // console.log(s);
+
 // 3 -т ХУВААГДАХ
 // let  a = 3;
 // let b = 12;
@@ -107,6 +117,7 @@
 // if(c % 3 == 0) s++;
 // if(d % 3 == 0) s++;
 // console.log(s);
+
 // 11 -т ХУВААГДАДГҮЙ ТООНУУДЫН НИЙЛБЭР
 // let  a = 7;
 // let b = 22;
@@ -118,6 +129,7 @@
 // if(c % 11 !== 0) s = s + c;
 // if(d % 11 !== 0) s = s + d;
 // console.log(s);
+
 // 10-с ИХ БОЛ YES ҮГҮЙ БОЛ NO
 // let  a = 11;
 // if(a > 10 ) console.log("YES");
@@ -126,19 +138,269 @@
 // let  a = 5;
 // if(a > 5 ) console.log("YES");
 // else {console.log("NO");}
-// Тэгш ТООНУУД YES
-let  a = 3;
-let b = 4;
-let c = 5;
-if(a % 2 == 0) console.log("YES");
-else {console.log("NO");
+
+// Тэгш ТООНУУД YES 
+// let  a = 3;
+// let b = 4;
+// let c = 5;
+// if(a % 2 == 0) console.log("YES");
+// else {console.log("NO");
+// }
+// if(b % 2 == 0) console.log("YES");
+// else {console.log("NO");
+// }
+// if(c % 2 == 0) console.log("YES");
+// else {console.log("NO");
+// }
+
+// Сондгой ТООНУУД YES 
+// let  a = 3;
+// let b = 4;
+// let c = 5;
+// if(a % 2 !== 0) console.log("YES");
+// else {console.log("NO");
+// }
+// if(b % 2 !== 0) console.log("YES");
+// else {console.log("NO");
+// }
+// if(c % 2 !== 0) console.log("YES");
+// else {console.log("NO");
+// }
+
+// Тоог эерэг, сөрөг, тэг байдлаар ангилах 
+// let  a = 3;
+// let b = -4;
+// let c = 0;
+// if( a > 0 ){
+//     console.log( a + " Эерэг тоо");
+// } else if( a < 0) {
+//     console.log( a + " Сөрөг тоо");
+// } else {
+//     console.log( a + " 0 тоо");
+// }
+// if( b > 0 ){
+//     console.log( b + " Эерэг тоо");
+// } else if( b < 0) {
+//     console.log( b + " Сөрөг тоо");
+// } else {
+//     console.log( b + " 0 тоо");
+// }
+// if( c > 0 ){
+//     console.log( c + " Эерэг тоо");
+// } else if( c < 0) {
+//     console.log( c + " Сөрөг тоо");
+// } else {
+//     console.log( c + " 0 тоо");
+// }
+
+// Нэг тоо нөгөө тоогоор хуваагддаг эсэхийг шалгах
+// let  a = 12;
+// let b = 4;
+// if(a % b == 0) console.log( a + " нь " + b + ' -т хуваагддаг');
+// else {
+//     console.log( a + "нь" + b + ' хуваагддаггүй');
+// }
+
+// Тоон оноог үсгийн дүн (A, B, C, D, F) болгон хувиргах дүнгийн тооцоолуур
+let dun = 98;
+if( dun <= 100 && dun >= 90) console.log( " A авсан байна");
+if(dun <= 89 && dun >= 80) console.log(" B авсан байна");
+if( dun <= 79 && dun >= 70) console.log(" C авсан байна");
+if( dun <= 69 && dun >= 60 ) console.log(" D авсан байна");
+if(dun <= 59 && dun >= 50) console.log(" F авсан байна");
+
+/* Цельсийг Фаренгейтын температурт хувиргах 
+ТОМЬЁО: Фаренгейт=(Цельс×9/5)+32 
+*/
+let Celis = 25;
+let farenget = (Celis * 9/5) + 32;
+console.log("Фаренгейтын температурт бол " + farenget);
+
+// Өгөгдсөн жил нь өндөр жил эсэхийг шалгах
+let year = 2024;
+if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log( year + ' он бол өндөр жил юм');
+} else {
+    console.log( year = " он бол өндөр жил биш юм")
 }
-if(b % 2 == 0) console.log("YES");
-else {console.log("NO");
+
+// Насны ангилал тодорхойлогч (Хүүхэд, Өсвөр үе, Насанд хүрсэн, Ахимаг нас)
+let age = 37;
+if( age <= 14 && age >= 6 ) console.log( "Та бол хүүхэд");
+if( age <= 17 && age >= 15 ) console.log( "Та бол өсвөр үеийн хүүхэд");
+if( age <= 49 && age >= 18 ) console.log( "Та бол насанд хүрсэн иргэн.");
+if( age <= 100 && age >= 50 ) console.log( "Та бол ахимаг настан.");
+
+// Худалдан авсан дүнгийн дагуу янз бүрийн хөнгөлөлтийн хувь хэрэглэдэг хөнгөлөлтийн тооцоолуур
+let une = 566;
+if( une <= 99  ) console.log( " Уучлаарай та хөнгөлөлт эдлэх боломжгүй байна. ");
+if( une <= 300 && une >= 100 ) console.log(" 30% -ын хөнгөлөлттэй. ");
+if( une <= 1000 && une >= 500 ) console.log( " 50% -ын хөнгөлөлттэй. ");
+
+// Өгөгдсөн гурван урт нь гурвалжин үүсгэх боломжтой эсэхийг шалгах
+let a = 5;
+let b = 5;
+let c = 8;
+if( a + b > c && a + c > b && b + c > a) console.log( " Гурвалжин үүсэх боломжтой ")
+    else {console.log( " Гурвалжин үүсэх боломжгүй ") }
+
+// Биеийн жингийн индекс тооцоолуур (Жин бага, Хэвийн, Илүү жинтэй, Тарган)
+let jin = 46;
+if( jin <= 45 ) console.log( " Жин бага");
+if( jin <= 79 && jin >= 46 ) console.log(" Хэвийн жинтэй ");
+if( jin >= 80) console.log(" Илүүдэл жинтэй");
+
+// Тэмдэгт эгшиг эсвэл гийгүүлэгч эсэхийг шалгах
+/* includes = string dotor todorhoi utga baigaa esehiig shalgadag boolen 
+*/
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let char = 'e';
+if (vowels.includes(char)) {
+    console.log(char + " нь эгшиг үсэг.");
+} else {
+    console.log(char + " нь гийгүүлэгч үсэг.");
 }
-if(c % 2 == 0) console.log("YES");
-else {console.log("NO");
+
+// Энгийн хувьтай зээлийн тооцоолуур
+/* Энгийн хүү тооцох томьёо:
+Хүү = Зээлийн дүн × Хүүгийн хувь × Хугацаа
+Нийт төлбөр = Зээлийн дүн + Хүү */
+let zeel = 1000000;
+let huwi = 12; 
+let hugatsaa = 2; 
+let huu = zeel * (huwi/100) * hugatsaa;
+let tulbur = zeel + huu;
+if( zeel >= 100000 && zeel <= 50000000) {
+    console.log( "Хүү нь: " + huu + " төгрөг");
+    console.log( "Нийт төлбөр: " + tulbur + " төгрөг");
 }
+
+// Тэнцсэн унасан
+// let too = 4;
+// if(too > 3) console.log("Тэнцсэн байна")
+//     else { console.log("Тэнцээгүй байна")
+// }
+
+// 5-c onts ruu
+let too = 4;
+if(too <= 5 && too >= 5 ) console.log("Онц");
+if(too <= 4 && too >= 4) console.log("Сайн");
+if(too <= 3 && too >= 3) console.log("Дунд");
+if(too <= 2 && too >= 2) console.log("Муу");
+
+// Garag
+// let n = 5;
+// if(n == 1)console.log( 'Monday');
+// if(n == 2)console.log( 'Thuesday');
+// if(n == 3)console.log( 'Wednesday');
+// if(n == 4)console.log( 'Thursday');
+// if(n == 5)console.log( 'Friday');
+// if(n == 6)console.log( 'Saturday');
+// if(n == 7)console.log( 'Sunday');
+
+// Uliral
+let u = 12;
+if(u == 11 || u == 12 || u == 1 )console.log( 'Winter');
+if(u == 2 || u == 3 || u == 4)console.log( 'Spring');
+if(u == 5 || u == 6 || u == 7)console.log( 'Summer');
+if(u == 8 || u == 9 || u == 10)console.log( 'Autumn');
+
+// Илэрхийлэл
+let B = 2;
+let C = 3;
+let D = 4;
+let A = B * C - D;
+console.log(A)
+
+// IOI 3 UDAA
+
+for(let i = 0; i < 3; i++ ){
+    console.log("IOI")
+}
+// IOI n udaa
+// let n = 8;
+// for(let i = 0; i <= n ; i++ ){
+//     console.log("IOI");
+// }
+// Ehnii n toonii niilber
+// let n = 5;
+// let niilber = 0;
+// for(i = 1; i <= n; i++ ){
+//     niilber = niilber + i
+// } console.log(niilber)
+
+// n!
+// let n = 5;
+// let urjwer = 1; 
+// for(i = 1; i <= n; i++){
+//     urjwer = urjwer * i
+// } console.log(urjwer)
+
+//  100 -g n udaa nem
+// let n = 6;
+// let niilber = 0;
+// for(i = 1; i <= n; i++ ){
+//     niilber = niilber + 100
+// } console.log(niilber)
+
+// a toog n udaa nem
+// let n = 6;
+// let niilber = 0;
+// for(i = 1; i <= n; i++ ){
+//     niilber = niilber + 50
+// } console.log(niilber)
+
+// 2-in n zereg
+// let n = 5;
+// let niilber = 1;
+// for(i = 1; i <= n; i++ ){
+//     niilber = niilber * 2
+// } console.log(niilber)
+
+// a tooni n zereg
+// let n = 4;
+// let niilber = 1;
+// for(i = 1; i <= n; i++ ){
+//     niilber = niilber * 3
+// } console.log(niilber)
+
+// Tooni hurd
+// let n = 3;
+// let hurd = 1;
+// for(i = 1; i <= 10; i++){
+//     hurd = n * i;
+//     console.log( n + " * " + i + ' = ' + hurd)
+// }
+
+// Toonii zergiin hurd
+let n = 5;
+let j = 3
+let hurd = 1;
+for(i = 1; i <= n; i++){
+    hurd = hurd * j;
+    console.log( j + " * " + i + ' = ' + hurd )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
 
 
 
